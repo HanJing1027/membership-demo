434a0fb7df6a6e2abad47c7ecb14f1889d4dd71e
HanJing1027 <71305380+HanJing1027@users.noreply.github.com>
2025-04-26 21:18:14 +0800

    fix: 修改API攔截器排除部分無需 token 的 api 請求

--- frontend/src/server/api/membershipApi.js
+++ frontend/src/server/api/membershipApi.js
@@ -7,10 +7,16 @@ const api = axios.create()
 const TOKEN_KEY = 'auth_token' // 需要和 vuex auth.js 中使用的常數一致
 
 // 登入、註冊不需要攜帶 JWT
-const EXCLUDED_URLS = ['/api/login', '/api/register']
+const EXCLUDED_URLS = [
+  '/api/register',
+  '/api/verify',
+  '/api/resendotp',
+  '/api/login',
+  '/api/forgot-password',
+]
 
 api.interceptors.request.use((config) => {
-  // 請求的 URL 不在排除列表中就攜帶 JWT
+  // 請求的 URL 不在排除列表中就攜帶 JWT Token
   if (!EXCLUDED_URLS.includes(config.url)) {
     const token = Cookies.get(TOKEN_KEY)
     if (token) {
@@ -58,10 +64,10 @@ export const membershipApi = {
     }
   },
 
-  // 登入
-  login: async (userData) => {
+  // 發送驗證 OTP 至註冊的 email
+  verifyOtp: async (otpCode) => {
     try {
-      const response = await api.post('/api/login', userData)
+      const response = await api.post('/api/verify', otpCode)
       return response.data
     } catch (error) {
       handleError(error)
@@ -69,10 +75,22 @@ export const membershipApi = {
     }
   },
 
-  // 登出
-  logout: async () => {
+  // 重送 OTP
+  resendOtp: async (emailData) => {
     try {
-      await api.post('/api/logout')
+      const response = await api.post('/api/resendotp', emailData)
+      return response.data
+    } catch (error) {
+      handleError(error)
+      throw error
+    }
+  },
+
+  // 登入
+  login: async (userData) => {
+    try {
+      const response = await api.post('/api/login', userData)
+      return response.data
     } catch (error) {
       handleError(error)
       throw error
@@ -90,29 +108,17 @@ export const membershipApi = {
     }
   },
 
-  // 重送 OTP
-  resendOtp: async (emailData) => {
+  // 登出
+  logout: async () => {
     try {
-      const response = await api.post('/api/resendotp', emailData)
-      return response.data
+      await api.post('/api/logout')
     } catch (error) {
       handleError(error)
       throw error
     }
   },
 
-  // 發送驗證 OTP 至註冊的 email
-  verifyOtp: async (otpCode) => {
-    try {
-      const response = await api.post('/api/verify', otpCode)
-      return response.data
-    } catch (error) {
-      handleError(error)
-      throw error
-    }
-  },
-
-  // 忘記密碼
+  // 忘記密碼 (發送重設密碼的 email)
   forgotPassword: async (emailData) => {
     try {
       const response = await api.post('/api/forgot-password', emailData)
